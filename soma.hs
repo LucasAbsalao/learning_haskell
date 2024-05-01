6bf7ea926e46d64a666d668bc1960ff99628209c
quinhas04 <lsa4@cin.ufpe.br>
2024-05-01 16:02:38 -0300

    better maquinasomar

--- soma.hs
+++ soma.hs
@@ -11,16 +11,11 @@ nextList(a:as) = nextList as
 maquinaSomar :: [Int] -> [Int]
 maquinaSomar [] = []
 maquinaSomar (0:0:as) = []
-maquinaSomar (0:as) 
+maquinaSomar (a:as) 
     | as==[] = [] ++ maquinaSomar as
+    | a/=0 = [soma (a:as)] ++ maquinaSomar (nextList as)
     | otherwise = [soma as] ++ maquinaSomar (nextList as)
-maquinaSomar (a:as) = [soma (a:as)] ++ maquinaSomar (nextList as)
 
 main = do
        lista <- getLine
        print $ maquinaSomar (read lista :: [Int])
-
--- maquinaSomar (0:as) 
---     | as==[] = [] ++ maquinaSomar as
---     | otherwise = [soma as] ++ maquinaSomar as
--- maquinaSomar (a:as) = maquinaSomar as
