d5dcb9aa7beb28fae87bcaf339198c8ec09d6db8
quinhas04 <lsa4@cin.ufpe.br>
2024-05-11 11:50:07 -0300

    estatisticas

--- estatisticasDaFaturaCartao.hs
+++ estatisticasDaFaturaCartao.hs
@@ -1,14 +1,29 @@
+main = do
+    a <- getLine
+    let result = minMaxCartao a
+    print result
+
+    
 stringToFloat :: String -> String
 stringToFloat (a:as)
     | a==';' = ""
     | otherwise = a : stringToFloat as
 
 
-getValue :: String -> Int -> [Float] 
+getValue :: String -> Int -> [Double] 
 getValue [] i = []
 getValue (a:as) i
-    | i==1 = [read (stringToFloat (a:as)) :: Float] ++ getValue as 0
+    | i==3 = [read (stringToFloat (a:as)) :: Double] ++ getValue as 0
     | a==';' = getValue as (i+1)
     | otherwise = getValue as i
 
---minMaxCartao :: String -> (Double, Double)
+maxFloat :: [Double] -> Double
+maxFloat (a:[]) = a
+maxFloat (a:as) =  max a (maxFloat as)
+
+minFloat :: [Double] -> Double
+minFloat (a:[]) = a
+minFloat (a:as) =  min a (minFloat as)
+
+minMaxCartao :: String -> (Double, Double)
+minMaxCartao a = (minFloat (getValue a 1), maxFloat (getValue a 1))
