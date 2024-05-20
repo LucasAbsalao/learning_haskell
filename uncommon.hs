7653752c6d21c3d446bdbff6b7b584321e2e3281
lsa4 <lsa4@g4c17.cin.ufpe.br>
2024-05-20 15:08:15 -0300

    uncommon words terminada

--- uncommon.hs
+++ uncommon.hs
@@ -1,10 +1,28 @@
-stringLists :: String -> String -> String -> [String]
-stringLists [] [] actual = []
-stringLists (a:[]) str2 actual= [actual++[a]] ++ stringLists str2 [] ""
-stringLists (a:as) str2 actual
-    | a==' ' = [actual] ++ stringLists as str2 ""
-    | a>='A' && a<='Z' = stringLists as str2 (actual++[upperToLower a])
-    | otherwise = stringLists as str2 (actual++[a])
+-- stringLists :: String -> String -> String -> [String]
+-- stringLists [] [] actual = []
+-- stringLists (a:[]) str2 actual= [actual++[a]] ++ stringLists str2 [] ""
+-- stringLists (a:as) str2 actual
+--     | a==' ' = [actual] ++ stringLists as str2 ""
+--     | a>='A' && a<='Z' = stringLists as str2 (actual++[upperToLower a])
+--     | otherwise = stringLists as str2 (actual++[a])
+
+stringLists :: String -> String -> [String]
+stringLists "" "" = []
+stringLists "" str2 = stringLists str2 ""
+stringLists str1 str2 = [spanEspacos str1 str2] ++ stringLists (nextWord str1 False) str2
+
+nextWord :: String -> Bool -> String
+nextWord "" b = ""
+nextWord (' ':as) b = nextWord as True
+nextWord (a:as) True = (a:as) 
+nextWord (a:as) b = nextWord as b
+
+spanEspacos :: String -> String -> String
+spanEspacos "" str2 = "" 
+spanEspacos (' ':as) str2= "" 
+spanEspacos (a:as) str2
+    | a>='A' && a<='Z' = (upperToLower a) : spanEspacos as str2
+    | otherwise = a : spanEspacos as str2
 
 upperToLower :: Char -> Char
 upperToLower a = toEnum (fromEnum a + fromEnum ' ')
@@ -28,7 +46,7 @@ uncommonWords (a:as)
     | otherwise = uncommonWords (removeWord as a)
 
 uncommonFromTwoSentences :: String -> String -> [String]
-uncommonFromTwoSentences str1 str2 = qsort(uncommonWords (stringLists str1 str2 ""))
+uncommonFromTwoSentences str1 str2 = qsort(uncommonWords (stringLists str1 str2))
 
 qsort :: [String] -> [String]
 qsort [] = []
